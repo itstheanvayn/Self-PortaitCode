ce313d40ad85cef2a39c2d9b3bfc08d7f6572a63
itstheanvayn <itstheanvayn@gmail.com>
2025-10-23 09:55:07 -0700

    Add files via upload

---
+++ ps_j5portraitdraft/ps_j5portraitdraft.js
@@ -0,0 +1,52 @@
+function setup() { 
+  createCanvas(1000, 1000);
+
+  
+  
+  beginShape();
+
+// Add the first control point.
+curveVertex(53.9433, 57.3); 
+
+
+// Add the anchor points to draw between.
+curveVertex(53.97, 342.3617)
+curveVertex(525.8085, 290.2766) 
+curveVertex(280.7021,56.4043) 
+curveVertex(335.8511,550.7021)
+// Add the second control point.
+
+curveVertex(53.9433, 57.3); 
+
+
+endShape(); function setup() {
+  createCanvas(100, 100);
+
+  background(200);
+
+  // Create a p5.Color object using a named color.
+  let c = color('blue');
+
+  // Draw the ellipse.
+  fill(blue);
+  noStroke(None);
+  square(20, 20, 60);
+
+  describe('blue ellipse');
+}
+
+function setup() { 
+  createCanvas(1000, 1000);
+  
+  // Add the first control point.
+  
+curveVertex(313.128, 290.2770)
+  
+  // Add the anchor points to draw between.
+curveVertex(261.8277, 278.35); 
+curveVertex(310.5745, 280.0638)
+  
+  
+  beginShape(ellipse);
+
+}
